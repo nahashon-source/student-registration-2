19adb12848b3712a076aaf9a8b9717e0c0485ea4
nahashon <nashonmwendwa0@gmail.com>
2025-05-19 19:33:07 +0300

    created a controller to handle form submissions and define routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students' [StudentController::class, 'index']);
+Route::get('/students/create', [StudentController::class, 'create']);
+Route::get('/students', [StudentController::class, 'store']);
